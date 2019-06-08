2156088e4769873d8546ae7d4c12a600f9b53bd8
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-06-08 08:42:22 +0200

    Remove AssetInterfaces

--- src/Asset.ts
+++ src/Asset.ts
@@ -1,18 +1,13 @@
 import { AssetBundle } from "./AssetBundle";
-import { IAssetIntersection } from "./AssetInterfaces";
 import { AssetTypeName } from "./schemas/AssetTypeName";
 
 /** Defines the base of all classes that represent an asset. */
 export abstract class Asset {
-    public static readonly typeName = Asset.getPropertyName("type");
+    public static readonly typeName = "type";
 
     public abstract get type(): AssetTypeName | "";
 
     public bundle(): AssetBundle {
         throw new Error("Asset cannot be bundled.");
-    }
-
-    private static getPropertyName<T extends keyof IAssetIntersection>(name: T) {
-        return name;
     }
 }
