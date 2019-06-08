d2bc377618a731af4a2b3edd6390874cc92adaf3
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-06-08 08:55:57 +0200

    Remove dependency

--- src/Asset.ts
+++ src/Asset.ts
@@ -1,11 +1,8 @@
 import { AssetBundle } from "./AssetBundle";
-import { AssetTypeName } from "./schemas/AssetTypeName";
 
 /** Defines the base of all classes that represent an asset. */
 export abstract class Asset {
     public static readonly typeName = "type";
-
-    public abstract get type(): AssetTypeName | "";
 
     public bundle(): AssetBundle {
         throw new Error("Asset cannot be bundled.");
